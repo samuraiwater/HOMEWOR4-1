c135dac6ac5a08beb6e15f3a668e10fba2fe768e
satosatoninjya <satomi4330@gmail.com>
2019-05-22 23:56:06 +1000

    Materialize ファイルをダウンロードしてindex.phpを変更しました。 model.php にSQL文を追加。 エラーが出て動いてません。

--- model.php
+++ model.php
@@ -3,7 +3,7 @@ function getFilmData($fdatas){
 	//DBの接続情報
 	include_once('config_database.php');
 	//DBコネクタを生成
-	$Mysqli = new mysqli($host, $username, $password, $dbname);
+    $Mysqli = new mysqli($host, $username, $password, $dbname);
 	if ($Mysqli->connect_error) {
 			error_log($Mysqli->connect_error);
 			exit;
@@ -13,12 +13,15 @@ function getFilmData($fdatas){
 	if(!empty($fdatas['title'])){
 		$where[] = "title like '%{$fdatas['title']}%'";
 	}
-	if($where){
-		$whereSql = implode(' AND ', $where);
-		$sql = 'select * from film where ' . $whereSql ;
-	}else{
-		$sql = 'select * from film';
-	}
+
+    
+	 //if($where){
+		// $whereSql = implode(' AND ', $where);
+		 $sql="SELECT film.title,category.name FROM film JOIN film_category ON film.film_id = film_category.film_id JOIN category ON film.film_id = category.category_id";
+	// 	$sql = 'select * from film where ' . $whereSql ;
+	// }else{
+	// 	$sql = 'select * from film';
+	//}
 	
 	//SQL文を実行する
 	$fdatas = $Mysqli->query($sql);
@@ -30,5 +33,13 @@ function getFilmData($fdatas){
     return $result;
     // echo $row;
 }
-
+  
+   //あらかじめ配列を生成しておき、while文で回します。
+   $fdatas = array();
+   while($row = $fdatas->fetch(PDO::FETCH_ASSOC)){
+	$fdatas[]=array(
+	 'title' =>$row['title'],
+	 'category'=>$row['category']
+	);
+   }  
 ?>
