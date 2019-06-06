b02e4da1ec67c86ca848f9bec9ae87f6737c3175
satosatoninjya <satomi4330@gmail.com>
2019-06-06 23:23:09 +1000

    modified $sql

--- model.php
+++ model.php
@@ -8,9 +8,7 @@ function getFilmData($fdatas){
 		error_log($Mysqli->connect_error);
 		exit;
 	}
-
-	//入力された検索条件からSQl文を生成
-	
+	//入力された検索条件からSQl文を生成	
 	$where = [];
 	if(!empty($fdatas['title'])){
 		$where[] = "title like '%{$fdatas['title']}%'";
@@ -20,28 +18,36 @@ function getFilmData($fdatas){
 	}
 	if(isset($where)){
 		$whereSql = implode(' AND ', $where);
-		    $sql = "SELECT film.title,category.name
-			FROM film 
-			JOIN film_category ON film.film_id = film_category.film_id 
-			JOIN category ON film_category.category_id = category.category_id 
-			`where` .  $whereSql
-			ORDER BY film.film_id";
+		$sql = 'SELECT film.title,category.name
+        FROM film
+        JOIN film_category ON film.film_id = film_category.film_id
+        JOIN category ON film_category.category_id = category.category_id';
+        if (!empty($whereSql)) {
+			$sql .= ' WHERE film.title = '. $whereSql;		
+		}
+		$sql .= ' ORDER BY film.film_id';
 	}else{
 		$sql = 'select * from film, category.name';
-
 	}
+
+	echo 'where =' . $where . '<br />';
+	echo '<br>';
+	echo 'fdatas =' . $fdatas . '<br />';
+	echo '<br>';
 	echo 'sql = ' . $sql . '<br>';
+	echo '<br>';
     echo 'whereSql = ' . $whereSql . '<br>';
 
-		
     //SQL文を実行する
-    $filmDataSet = $Mysqli->query($sql);
-    //扱いやすい形に変える
+	$filmDataSet = $Mysqli->query($sql);
+	//扱いやすい形に変える
     $result = [];
     while($row = $filmDataSet->fetch_assoc()){
         $result[] = $row;
-    }
+	}
     return $result;
 }
+
+echo 'row='. $row . '<br />';
 ?>
 
